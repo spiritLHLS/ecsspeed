4108c94a58435e4b8c49a886b2a7c8778cac997d
spiritLHLS <103393591+spiritLHLS@users.noreply.github.com>
2023-05-11 22:14:10 +0800

    Update ecsspeed-ping.sh

--- script/ecsspeed-ping.sh
+++ script/ecsspeed-ping.sh
@@ -47,28 +47,6 @@ checkroot(){
 	[[ $EUID -ne 0 ]] && echo -e "${RED}请使用 root 用户运行本脚本！${PLAIN}" && exit 1
 }
 
-checksystem() {
-	if [ -f /etc/redhat-release ]; then
-	    release="centos"
-	elif cat /etc/issue | grep -Eqi "debian"; then
-	    release="debian"
-	elif cat /etc/issue | grep -Eqi "ubuntu"; then
-	    release="ubuntu"
-	elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-	    release="centos"
-	elif cat /proc/version | grep -Eqi "debian"; then
-	    release="debian"
-	elif cat /proc/version | grep -Eqi "ubuntu"; then
-	    release="ubuntu"
-	elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-	    release="centos"
-    elif cat /etc/os-release | grep -Eqi "almalinux"; then
-        release="centos"
-    elif cat /proc/version | grep -Eqi "arch"; then
-        release="arch"
-	fi
-}
-
 checkupdate(){
 	    _yellow "Updating package management sources"
 		${PACKAGE_UPDATE[int]} > /dev/null 2>&1
@@ -125,137 +103,6 @@ check_china(){
             esac
         fi
     fi
-}
-
-SystemInfo_GetOSRelease() {
-    _yellow "checking OS"
-    if [ -f "/etc/centos-release" ]; then # CentOS
-        Var_OSRelease="centos"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3,$4}')"
-        if [ "$(rpm -qa | grep -o el6 | sort -u)" = "el6" ]; then
-            Var_CentOSELRepoVersion="6"
-            local Var_OSReleaseVersion="$(cat /etc/centos-release | awk '{print $3}')"
-        elif [ "$(rpm -qa | grep -o el7 | sort -u)" = "el7" ]; then
-            Var_CentOSELRepoVersion="7"
-            local Var_OSReleaseVersion="$(cat /etc/centos-release | awk '{print $4}')"
-        elif [ "$(rpm -qa | grep -o el8 | sort -u)" = "el8" ]; then
-            Var_CentOSELRepoVersion="8"
-            local Var_OSReleaseVersion="$(cat /etc/centos-release | awk '{print $4}')"
-        else
-            local Var_CentOSELRepoVersion="unknown"
-            local Var_OSReleaseVersion="<Unknown Release>"
-        fi
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-    elif [ -f "/etc/fedora-release" ]; then # Fedora
-        Var_OSRelease="fedora"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3}')"
-        local Var_OSReleaseVersion="$(cat /etc/fedora-release | awk '{print $3,$4,$5,$6,$7}')"
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-    elif [ -f "/etc/redhat-release" ]; then # RedHat
-        Var_OSRelease="rhel"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3,$4}')"
-        if [ "$(rpm -qa | grep -o el6 | sort -u)" = "el6" ]; then
-            Var_RedHatELRepoVersion="6"
-            local Var_OSReleaseVersion="$(cat /etc/redhat-release | awk '{print $3}')"
-        elif [ "$(rpm -qa | grep -o el7 | sort -u)" = "el7" ]; then
-            Var_RedHatELRepoVersion="7"
-            local Var_OSReleaseVersion="$(cat /etc/redhat-release | awk '{print $4}')"
-        elif [ "$(rpm -qa | grep -o el8 | sort -u)" = "el8" ]; then
-            Var_RedHatELRepoVersion="8"
-            local Var_OSReleaseVersion="$(cat /etc/redhat-release | awk '{print $4}')"
-        else
-            local Var_RedHatELRepoVersion="unknown"
-            local Var_OSReleaseVersion="<Unknown Release>"
-        fi
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-    elif [ -f "/etc/lsb-release" ]; then # Ubuntu
-        Var_OSRelease="ubuntu"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/NAME/{print $3}' | head -n1)"
-        local Var_OSReleaseVersion="$(cat /etc/os-release | awk -F '[= "]' '/VERSION/{print $3,$4,$5,$6,$7}' | head -n1)"
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-        Var_OSReleaseVersion_Short="$(cat /etc/lsb-release | awk -F '[= "]' '/DISTRIB_RELEASE/{print $2}')"
-    elif [ -f "/etc/debian_version" ]; then # Debian
-        Var_OSRelease="debian"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3,$4}')"
-        local Var_OSReleaseVersion="$(cat /etc/debian_version | awk '{print $1}')"
-        local Var_OSReleaseVersionShort="$(cat /etc/debian_version | awk '{printf "%d\n",$1}')"
-        if [ "${Var_OSReleaseVersionShort}" = "7" ]; then
-            Var_OSReleaseVersion_Short="7"
-            Var_OSReleaseVersion_Codename="wheezy"
-            local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Wheezy\""
-        elif [ "${Var_OSReleaseVersionShort}" = "8" ]; then
-            Var_OSReleaseVersion_Short="8"
-            Var_OSReleaseVersion_Codename="jessie"
-            local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Jessie\""
-        elif [ "${Var_OSReleaseVersionShort}" = "9" ]; then
-            Var_OSReleaseVersion_Short="9"
-            Var_OSReleaseVersion_Codename="stretch"
-            local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Stretch\""
-        elif [ "${Var_OSReleaseVersionShort}" = "10" ]; then
-            Var_OSReleaseVersion_Short="10"
-            Var_OSReleaseVersion_Codename="buster"
-            local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Buster\""
-        else
-            Var_OSReleaseVersion_Short="sid"
-            Var_OSReleaseVersion_Codename="sid"
-            local Var_OSReleaseFullName="${Var_OSReleaseFullName} \"Sid (Testing)\""
-        fi
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-    elif [ -f "/etc/alpine-release" ]; then # Alpine Linux
-        Var_OSRelease="alpinelinux"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/NAME/{print $3,$4}' | head -n1)"
-        local Var_OSReleaseVersion="$(cat /etc/alpine-release | awk '{print $1}')"
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-    elif [ -f "/etc/almalinux-release" ]; then # almalinux
-        Var_OSRelease="almalinux"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3}')"
-        local Var_OSReleaseVersion="$(cat /etc/almalinux-release | awk '{print $3,$4,$5,$6,$7}')"
-        local Var_OSReleaseArch="$(arch)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName $Var_OSReleaseVersion ($Var_OSReleaseArch)"
-    elif [ -f "/etc/arch-release" ]; then # archlinux
-        Var_OSRelease="arch"
-        local Var_OSReleaseFullName="$(cat /etc/os-release | awk -F '[= "]' '/PRETTY_NAME/{print $3}')"
-        local Var_OSReleaseArch="$(uname -m)"
-        LBench_Result_OSReleaseFullName="$Var_OSReleaseFullName ($Var_OSReleaseArch)" # 滚动发行版 不存在版本号
-    else
-        Var_OSRelease="unknown" # 未知系统分支
-        LBench_Result_OSReleaseFullName="[Error: Unknown Linux Branch !]"
-    fi
-}
-
-
-SystemInfo_GetSystemBit() {
-    _yellow "checking SystemBit"
-    local sysarch="$(uname -m)"
-    if [ "${sysarch}" = "unknown" ] || [ "${sysarch}" = "" ]; then
-        local sysarch="$(arch)"
-    fi
-    # 根据架构信息设置系统位数并下载文件,其余 * 包括了 x86_64
-    case "${sysarch}" in
-        "i386" | "i686")
-            LBench_Result_SystemBit_Short="32"
-            LBench_Result_SystemBit_Full="i386"
-            BESTTRACE_FILE=besttracemac
-            ;;
-        "armv7l" | "armv8" | "armv8l" | "aarch64")
-            LBench_Result_SystemBit_Short="arm"
-            LBench_Result_SystemBit_Full="arm"
-            BESTTRACE_FILE=besttracearm
-            BACKTRACE_FILE=backtrace-linux-arm64.tar.gz
-            ;;
-        *)
-            LBench_Result_SystemBit_Short="64"
-            LBench_Result_SystemBit_Full="amd64"
-            BESTTRACE_FILE=besttrace
-            BACKTRACE_FILE=backtrace-linux-amd64.tar.gz
-            ;;
-    esac
 }
 
 ping_test() {
@@ -417,12 +264,9 @@ print_end_time() {
 }
 
 checkroot
-checksystem
 # checkupdate
 checkcurl
 checkwget
-SystemInfo_GetOSRelease
-SystemInfo_GetSystemBit
 check_cdn_file
 check_china
 SERVER_BASE_URL="https://raw.githubusercontent.com/spiritLHLS/speedtest.net-CN-ID/main"
