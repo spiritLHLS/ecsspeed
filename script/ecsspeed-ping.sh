0cfcd8b3d171d972d39a54eab129753b114f0d4f
spiritlhls <tg@spiritlhl.top>
2023-10-22 09:26:47 +0000

    修改 ecsspeed-ping.sh

--- script/ecsspeed-ping.sh
+++ script/ecsspeed-ping.sh
@@ -352,7 +352,7 @@ get_string_length() {
     echo $length
 }
 
-cdn_urls=("https://cdn.spiritlhl.workers.dev/" "https://cdn3.spiritlhl.net/" "https://cdn1.spiritlhl.net/" "https://ghproxy.com/" "https://cdn2.spiritlhl.net/")
+cdn_urls=("https://cdn0.spiritlhl.top/" "https://cdn3.spiritlhl.net/" "https://cdn1.spiritlhl.net/" "https://ghproxy.com/" "https://cdn2.spiritlhl.net/")
 
 check_cdn() {
     _yellow "checking CDN"
